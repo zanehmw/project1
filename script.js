16ede7ea37aae9046d6ee757a350bd08ade2e031
Zaneh Williams <zanehmw@gmail.com>
2016-03-28 08:58:35 -0400

    got homework done

---
+++ script.js
@@ -0,0 +1,23 @@
+// trying to make a deck of cards
+var Flashcards = [
+  { cardPosition: "1"
+    eWord: "Hello"
+    kWord: "안녕하세요"
+    answered:
+    correct:
+  },
+  {
+    cardPosition: "2"
+    eWord: ""
+    kWord: ""
+    answered:
+    correct:
+  },
+  {
+    cardPosition: "3"
+    eWord: ""
+    kWord: ""
+    answered:
+    correct:
+  },
+]
