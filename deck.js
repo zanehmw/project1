a6f5cfdab7cb2f38932305e993dd013e580acb4c
Zaneh Williams <zanehmw@gmail.com>
2016-03-29 14:59:44 -0400

    did some refactoring, moved deck to new file

---
+++ deck.js
@@ -0,0 +1,31 @@
+var deck = [
+  {
+    eWord: "Hello",
+    kWord: "안녕하세요",
+    correct: "false",
+  },
+
+  {
+    eWord: "excited",
+    kWord: "신났다",
+    correct: "false",
+  },
+
+  {
+    eWord: "What is your name?",
+    kWord: "이름이 뭐예요?",
+    correct: "false",
+  },
+
+  {
+    eWord: "poop",
+    kWord: "똥",
+    correct: "false",
+  },
+
+  {
+    eWord: "That man is good-looking",
+    kWord: "그남자가 잘 생겼다",
+    correct: "false",
+  }
+];
--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-// trying to make a deck of cards
-var deck = [
-  {
-    eWord: "Hello",
-    kWord: "안녕하세요",
-    correct: "false",
-  },
-
-  {
-    eWord: "excited",
-    kWord: "신났다",
-    correct: "false",
-  },
-
-  {
-    eWord: "What is your name?",
-    kWord: "이름이 뭐예요?",
-    correct: "false",
-  },
-  {
-    eWord: "poop",
-    kWord: "똥",
-    correct: "false",
-  },
-  {
-    eWord: "That man is good-looking",
-    kWord: "그남자가 잘 생겼다",
-    correct: "false",
-  }
-];
-
 var cardDisplay = document.querySelector("#flash_card")
 var current = 0
 var currentCard = deck[current]
+var correctCards= []
+var correctButton = document.querySelector("#correctButton")
+var previousButton = document.querySelector("#previousButton")
+var nextButton = document.querySelector("#nextButton")
+var incorrectButton = document.querySelector("#incorrectButton")
+
 cardDisplay.innerHTML = currentCard.eWord
-//this function shows english word if korean word is displayed and vice versa
+
+cardDisplay.addEventListener( "click", flipCard );
+nextButton.addEventListener( "click", nextCard );
+previousButton.addEventListener( "click", previousCard );
+correctButton.addEventListener( "click", selectCorrect );
+incorrectButton.addEventListener( "click", selectIncorrect );
+
 function flipCard() {
   if (cardDisplay.innerHTML == currentCard.eWord) {
     cardDisplay.innerHTML = currentCard.kWord
@@ -42,54 +23,33 @@ function flipCard() {
     cardDisplay.innerHTML = currentCard.eWord
   }
 };
-cardDisplay.addEventListener( "click", flipCard );
-
-//creating a new event listener and function to change the card current card using the next card button
-var nextButton = document.querySelector("#nextButton")
-nextButton.addEventListener( "click", nextCard );
 
 function nextCard () {
   current = current + 1
   currentCard  = deck[current]
   cardDisplay.innerHTML = currentCard.eWord
-}
-
-//creating a new event listener for the previous card button
-
-var previousButton = document.querySelector("#previousButton")
-previousButton.addEventListener( "click", previousCard );
+};
 
 function previousCard () {
   current = current - 1
   currentCard  = deck[current]
   cardDisplay.innerHTML = currentCard.eWord
-}
-
-//creating an event listener for the correct and incorrect buttons
-var correctCards= []
-
-var correctButton = document.querySelector("#correctButton")
-correctButton.addEventListener( "click", selectCorrect );
+};
 
 function selectCorrect () {
   console.log("poop")
   currentCard.correct = "true";
   moveCorrect()
+  nextCard()
 };
-
-var incorrectButton = document.querySelector("#incorrectButton")
-incorrectButton.addEventListener( "click", selectIncorrect );
 
 function selectIncorrect () {
   console.log("poop alot")
-
 };
-
-// attempting to push correct cards to a different array in the function below
 
 function moveCorrect() {
   if (currentCard.correct = "true") {
     correctCards.push(currentCard);
     deck.splice(current, 1);
   }
-}
+};
